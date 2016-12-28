2a053f2dee48ac37f71fcf218709daf79b0313a6
小冉 <hoter@xunhuweb.com>
2016-12-28 14:42:10 +0800

    add remark
    add remark

--- class-wechat-wc-payment-gateway.php
+++ class-wechat-wc-payment-gateway.php
@@ -217,14 +217,16 @@ class XH_Wechat_Payment_WC_Payment_Gateway extends WC_Payment_Gateway {
     					'type'        => 'text',
     					'css'         => 'width:400px',
     					'default'     => '',
-    					'section'     => 'default'
+    					'section'     => 'default',
+                        'description' =>__('<a target="_blank" href="http://mp.wordpressopen.com">register and get app id</a>.', XH_Wechat_Payment )
 				),
 				'appsecret' => array(
     					'title'       => __( 'APP Secret', XH_Wechat_Payment ),
     					'type'        => 'text',
     					'css'         => 'width:400px',
     					'default'     => '',
-    					'section'     => 'default'
+    					'section'     => 'default',
+                        'description' =>__('<a target="_blank" href="http://mp.wordpressopen.com">register and get app secret</a>.', XH_Wechat_Payment )
 				),
 				'transaction_url' => array(
     					'title'       => __( 'Transaction Url', XH_Wechat_Payment ),
